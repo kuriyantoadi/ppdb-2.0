984534763fce2ba57b275fc2bb7be36ecff09173
kuriyantoadi <melatikuning212@gmail.com>
2021-06-16 14:39:38 +0700

    update 16 juni 2021, update 6

--- daftar/pendaftar.php
+++ daftar/pendaftar.php
@@ -36,20 +36,20 @@
   include '../koneksi.php';
 
   //akl hari ke-1 laki-laki
-  $data_akl_h1 = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='14-06-2021' and jenis_kelamin='Laki-laki' ");
-  $jml_akl_h1_l = mysqli_num_rows($data_akl_h1);
+  $data_akl_h1_l = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='14-06-2021' and jenis_kelamin='Laki-laki' ");
+  $jml_akl_h1_l = mysqli_num_rows($data_akl_h1_l);
 
   //akl hari ke-1 perempuan
-  $data_akl_h1 = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='14-06-2021' and jenis_kelamin='Perempuan' ");
-  $jml_akl_h1_p = mysqli_num_rows($data_akl_h1);
+  $data_akl_h1_p = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='14-06-2021' and jenis_kelamin='Perempuan' ");
+  $jml_akl_h1_p = mysqli_num_rows($data_akl_h1_p);
 
   //akl hari ke-2 laki-laki
-  $data_akl_h2 = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='15-06-2021' and jenis_kelamin='Laki-laki' ");
-  $jml_akl_h2_l = mysqli_num_rows($data_akl_h2);
+  $data_akl_h2_l = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='15-06-2021' and jenis_kelamin='Laki-laki' ");
+  $jml_akl_h2_l = mysqli_num_rows($data_akl_h2_l);
 
   //akl hari ke-2 perempuan
-  $data_akl_h2 = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='15-06-2021' and jenis_kelamin='Perempuan' ");
-  $jml_akl_h2_p = mysqli_num_rows($data_akl_h2);
+  $data_akl_h2_p = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='15-06-2021' and jenis_kelamin='Perempuan' ");
+  $jml_akl_h2_p = mysqli_num_rows($data_akl_h2_p);
 
   //akl hari ke-3 Laki-laki
   $data_akl_h3 = mysqli_query($koneksi, "SELECT * FROM f_siswa_akl where tgl_pendaftaran='16-06-2021' and jenis_kelamin='Laki-laki'");
@@ -76,7 +76,7 @@
   $jml_akl_l = $jml_akl_h1_l+$jml_akl_h2_l+$jml_akl_h3_l+$jml_akl_h4_l+$jml_akl_h5_l;
   $jml_akl_p = $jml_akl_h1_p + $jml_akl_h2_p + $jml_akl_h3_p + $jml_akl_h4_p + $jml_akl_h5_p;
 
-  
+
 
   ?>
 
